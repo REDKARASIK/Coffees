b3e75c0d840a7d282224b956ac3542833df0b2eb
REDKARASIK <redkarasik@yandex.ru>
2022-11-23 14:16:46 +1000

    Make only form of adding item

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 import sys
 import sqlite3
-from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem, QAbstractItemView
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem, QAbstractItemView, QDialog
 from mainUI import Ui_MainWindow
+from addEditCoffeeFrom import Ui_Dialog
 
 
 def main():
+    class AddCoffee(QDialog, Ui_Dialog):
+        def __init__(self):
+            super().__init__()
+            self.setupUi(self)
+            self.make_comboboxes()
+
+        def make_comboboxes(self):
+            with sqlite3.connect('coffee.sqlite3') as file:
+                # Combobox_sort
+                result = file.cursor().execute('select sorts.name from sorts').fetchall()
+                for i in result:
+                    self.sort_box.addItem(*i)
+                # Combobox_degree
+                result = file.cursor().execute('select degree.name from degree').fetchall()
+                for i in result:
+                    self.degree_box.addItem(*i)
+                # Grounded_box
+                self.ground_box.addItem('0')
+                self.ground_box.addItem('1')
+
     class CoffeeShow(QMainWindow, Ui_MainWindow):
         def __init__(self):
             super().__init__()
             self.setupUi(self)
             self.load_table()
             self.coffee_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+            self.add_button.clicked.connect(self.add_coffee)
 
         def load_table(self):
             self.coffee_table.setRowCount(0)
@@ -28,6 +50,12 @@ def main():
                     self.coffee_table.setRowCount(self.coffee_table.rowCount() + 1)
                     for j, elem in enumerate(row):
                         self.coffee_table.setItem(i, j, QTableWidgetItem(str(elem)))
+
+        def add_coffee(self):
+            dialog = AddCoffee()
+            dialog.show()
+            if dialog.exec():
+                print('Данные изменены')
 
     app = QApplication(sys.argv)
     ex = CoffeeShow()
